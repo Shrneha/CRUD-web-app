9d3bd26322c94f9fa3752694a61f78fe6dedf090
Shraddha bhuran <shraddha.bhuran31@gmail.com>
2020-05-12 19:19:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+
+from flask import Flask
+import sqlite3 as sql
+from flask import render_template ,request ,flash
+import sqlite3
+con = sql.connect('users.db')
+c =  con.cursor()
+
+app = Flask(__name__)
+#app.secret_key = "super secret key"
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    return render_template('index1.html')
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    error = None
+    response = False
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        con = sql.connect('users.db')
+        with con:
+            c = con.cursor()
+            c.execute("SELECT * FROM users")
+            rows = c.fetchall()
+            for row in rows :
+                user = row[1]
+                pwd = row[2]
+                if user == username and pwd == password :
+                    return render_template('logged_in.html')
+                else :
+                    return render_template('index1.html')
+            
+if __name__ == '__main__' :
+    app.run(debug=True )
+
+                
+            
+                      
+                      
