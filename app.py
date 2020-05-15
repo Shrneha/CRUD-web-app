e4359374edf28f61b8c3f9f17ef2e417449da7df
Shraddha bhuran <shraddha.bhuran31@gmail.com>
2020-05-15 18:53:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@
 from flask import Flask
 import sqlite3 as sql
 from flask import render_template ,request ,flash
-import sqlite3
 con = sql.connect('users.db')
 c =  con.cursor()
 
 app = Flask(__name__)
-
+app.secret_key = 'my super secret key'
 
 @app.route('/',methods=['GET','POST'])
 def home():
@@ -21,11 +20,16 @@ def login():
         con = sql.connect('users.db')
         with con:
             c = con.cursor()
-            c.execute('''SELECT * FROM users WHERE username = (?) and password =(?) ''',(username,password, ))
+            c.execute('''SELECT * FROM users WHERE username = (?) ''',(username,))
             rows = c.fetchone()
-            if rows[1] == username and rows[2] == password :
-                return render_template('logged_in.html')
+            if rows != None:
+                if rows[1] == username and rows[2] == password :
+                    return render_template('logged_in.html')
+                else :
+                    flash("Plese enter correct password")
+                    return render_template('index1.html')        
             else :
+                flash("Invalid Username")
                 return render_template('index1.html')
                 
 @app.route('/sign',methods = ['GET','POST'])
@@ -41,17 +45,21 @@ def signup():
         con = sql.connect('users.db')
         with con:
             c = con.cursor()
-            if password == repassword :
-                c.execute('''INSERT INTO users (username ,password) VALUES (?,?)''',(username,password))
-                return render_template("index1.html")
+            c.execute(''' SELECT * FROM users WHERE username LIKE (?) ''',( username,))
+            row = c.fetchone()
+            if row == None :
+                if password == repassword :
+                    c.execute('''INSERT INTO users (username ,password) VALUES (?,?)''',(username,password))
+                    return render_template("index1.html")
+                else :
+                    flash("Password does not match ")
+                    return render_template('signup.html')
             else :
+                flash ("Username already exists")
                 return render_template('signup.html')
 
-
+                           
 if __name__ == '__main__' :
     app.run(debug=True )
 
-                
-            
-                      
-                      
+                                
