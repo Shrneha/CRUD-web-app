146bdf945724259cb8223907729ae2364ff3502e
Shraddha bhuran <shraddha.bhuran31@gmail.com>
2020-05-19 12:13:30 +0530

    Update sqlitedb.py

--- sqlitedb.py
+++ sqlitedb.py
@@ -5,7 +5,6 @@ print("opened db successfully");
 print(con)
 
 c =  con.cursor()
-
 # Create table - Users
 c.execute('''drop table if exists users ''')
 c.execute('''    CREATE TABLE users(
@@ -14,14 +13,54 @@ c.execute('''    CREATE TABLE users(
         password  NOT NULL
         );''')
 c.execute('''INSERT INTO users(username,password)
-              VALUES ('admin','100')''')
+              VALUES ('admin','admin')''')
 c.execute('''INSERT INTO users(username,password)
-              VALUES ('user1','test')''')
+              VALUES ('user1','100')''')
+c.execute('''INSERT INTO users(username,password)
+             VALUES ('user2','200')''')
 
-#fetch all data from table:
+#fetch all data from users table:
 c.execute("SELECT * FROM users ")
 rows = c.fetchall()
 print(rows)
+
+# Create table departments
+c.execute(''' drop table if exists departments ''')
+c.execute(''' CREATE TABLE departments (
+               dept_id INTEGER PRIMARY KEY ,
+               dept_name TEXT NOT NULL ); ''')
+c.execute(''' INSERT INTO departments(dept_name)
+                VALUES ('admin')''')
+c.execute(''' INSERT INTO departments(dept_name)
+                VALUES ('sales')''')
+c.execute(''' INSERT INTO departments(dept_name)
+                VALUES ('development')''')
+
+# Fetch all data from departments 
+c.execute("SELECT * FROM departments ")
+rows = c.fetchall()
+print(rows)
+
+#Create table user_profile
+c.execute (''' drop table if exists user_profile ''')
+c.execute (''' CREATE TABLE user_profile (
+                prof_id INTEGER PRIMARY KEY ,
+                prof_name TEXT ,
+                is_admin NUMBER ); ''')
+c.execute(''' INSERT INTO user_profile(prof_name,is_admin)
+                VALUES ('Manager',1)''')
+c.execute(''' INSERT INTO user_profile(prof_name,is_admin)
+                VALUES ('Sales',0)''')
+c.execute(''' INSERT INTO user_profile(prof_name,is_admin)
+                VALUES ('Developer',0)''')
+
+#Fetch all data from user_profile
+c.execute("SELECT * FROM user_profile")
+rows = c.fetchall()
+print(rows)
+
+
+
 
 con.commit()
 con.close()
