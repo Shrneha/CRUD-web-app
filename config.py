57f6d0b4f76e4aeef9b76b0f0987749c2205c0dc
Shrneha <51981339+Shrneha@users.noreply.github.com>
2020-05-20 16:32:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ class Config(object):
     DEBUG = True
     TESTING = True
     SECRET_KEY = "My Super Secret Key"
+    SERVER_NAME = "127.0.0.1:5000"
     SESSION_COOKIE_SECURE = True
 
 class ProductionConfig(Config):
@@ -10,9 +11,9 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     DEBUG = True
     SESSION_COOKIE_SECURE = False
-
+    
 class TestingConfig(Config):
     TESTING = True
     SESSION_COOKIE_SECURE = False
-
+    
     
