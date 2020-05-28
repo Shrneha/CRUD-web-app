080cdddffa7d73bc15b10cfa1872c80d64f51657
Shraddha bhuran <shraddha.bhuran31@gmail.com>
2020-05-28 17:20:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app.config['MYSQL_DB'] = 'test'
 app.config['MYSQL_DATABASE_PORT']= '3306'
 app.config['SECRET_KEY'] = "My Super Secret Key"
 mysql = MySQL(app)
+#cur = mysql.connection.cursor()
 
 @app.route('/session/emp',methods=['GET','POST'])
 def list_employees():
@@ -23,12 +24,31 @@ def list_employees():
         cur.execute("SELECT * FROM users")
         result = cur.fetchall()
         cur.close()
-        return render_template('employees.html', users=result, username = username)
+        return render_template('admin_employees.html', users=result, username = username)
     else :
         cur.execute("SELECT * FROM users where username = %s",(username,) )
         result = cur.fetchall()
         cur.close()
         return render_template('employees.html', users=result, username = username)
+
+@app.route('/session/dept',methods =['GET','POST'])
+def list_dept():
+    username = session["username"]
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM departments")
+    result = cur.fetchall()
+    cur.close()
+    return render_template('admin_dept.html',users = result ,username = username)
+
+@app.route('/session/userprofile',methods =['GET','POST'])
+def list_userprofile():
+    username = session["username"]
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM userprofile")
+    result = cur.fetchall()
+    cur.close()
+    return render_template('admin_userprofile.html',users = result ,username = username)
+
 
 
 @app.route('/update', methods=["POST"])
@@ -45,23 +65,87 @@ def update():
     mysql.connection.commit()
     return redirect(url_for('list_employees'))
 
+@app.route('/dept_update',methods = ['POST'])
+def dept_update():
+    dept_id = request.form['dept_id']
+    dept_name = request.form['dept_name']
+    cur = mysql.connection.cursor()
+    cur.execute(''' UPDATE departments
+                    SET dept_id = %s , dept_name =%s WHERE dept_id =%s ''',(dept_id,dept_name,dept_id,))
+    mysql.connection.commit()
+    return redirect(url_for('list_dept'))
+
+@app.route('/userprofile_update',methods = ['POST'])
+def userprofile_update():
+    user_id = request.form['user_id']
+    prof_name = request.form['prof_name']
+    is_admin = request.form['is_admin']
+    cur = mysql.connection.cursor()
+    cur.execute(''' UPDATE userprofile
+                    SET user_id = %s , prof_name =%s , is_admin=%s WHERE user_id =%s ''',(user_id,prof_name,is_admin,user_id,))
+    mysql.connection.commit()
+    return redirect(url_for('list_userprofile'))
+
+
 @app.route('/delete/<string:id_data>', methods=["GET"])
 def delete(id_data):
     cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM users WHERE id=%s", (id_data,))
+    cur.execute(" DELETE FROM users WHERE id= %s ", (id_data,))
     mysql.connection.commit()
     return redirect(url_for('list_employees'))
 
+@app.route('/delete_dept/<string:dept_id>', methods=["GET"])
+def delete_dept(dept_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM departments WHERE dept_id = %s", (dept_id,))
+    mysql.connection.commit()
+    return redirect(url_for('list_dept'))
+
+@app.route('/delete_userprofile/<string:user_id>', methods=["GET"])
+def delete_userprof(user_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM userprofile WHERE user_id = %s ", (user_id,))
+    mysql.connection.commit()
+    return redirect(url_for('list_userprofile'))
+
+
+
+
 @app.route('/add_user',methods =["POST"])
 def add_user():
-    username = request.form['username']
-    password = request.form['password']
-    dept_id = request.form['dept_id']
-    user_id = request.form['user_id']
+    user = session["username"]
+    if 'admin' in user :
+        username = request.form['username']
+        password = request.form['password']
+        dept_id = request.form['dept_id']
+        user_id = request.form['user_id']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (username,password,dept_id,user_id) VALUES (%s,%s,%s,%s)",(username,password,dept_id,user_id))
+        mysql.connection.commit()
+        return redirect(url_for('list_employees'))
+    else :
+        flash("Access Denied!")
+        return redirect(url_for('list_employees'))
+
+@app.route('/add_dept',methods=['POST'])
+def add_dept():
+    dept_id = request.form["dept_id"]
+    dept_name = request.form["dept_name"]
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO users (username,password,dept_id,user_id) VALUES (%s,%s,%s,%s)",(username,password,dept_id,user_id))
+    cur.execute(" INSERT INTO departments (dept_id,dept_name) VALUES (%s,%s)",(dept_id,dept_name))
     mysql.connection.commit()
-    return redirect(url_for('list_employees'))
+    return redirect(url_for('list_dept'))
+
+@app.route('/add_userprofile',methods=['POST'])
+def add_userprofile():
+    user_id = request.form["user_id"]
+    prof_name = request.form["prof_name"]
+    is_admin = request.form["is_admin"]
+    cur = mysql.connection.cursor()
+    cur.execute(" INSERT INTO userprofile (user_id,prof_name,is_admin) VALUES (%s,%s,%s)",(dept_id,dept_name,is_admin))
+    mysql.connection.commit()
+    return redirect(url_for('list_userprofile'))
+
 
 
 ##@app.route('/update_emp', methods=['GET', 'POST'])
@@ -103,7 +187,10 @@ def login():
 def sess():
     if "username" in session:
         username = session["username"]
-        return render_template("logged_in.html" ,username = username)
+        if 'admin' in username :
+            return render_template("admin_logged_in.html", username = username)
+        else:
+            return render_template("logged_in.html" ,username = username)
     else:
         return render_template('index1.html')
     
@@ -146,5 +233,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug= True)
